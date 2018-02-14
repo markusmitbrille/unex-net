19266bf7b89248a05582f3c1d5aa8b47ad3d87d7
Markus Rezai <markus.rezai@gmail.com>
2018-02-14 12:59:18 +0100

    Implemented debug listener
    The behaviour adds a debug listener to the Debug.Listeners collection, which will relay all messages created by DebugUtils to the unity debug console.

---
+++ Scripts/Debugging/DebugListener.cs
@@ -0,0 +1,102 @@
+﻿using Autrage.LEX.NET;
+using System.Diagnostics;
+using UnityDebug = UnityEngine.Debug;
+
+internal class DebugListener : TraceListener
+{
+    public override void Write(string message) => UnityDebug.Log(message);
+
+    public override void Write(object o) => UnityDebug.Log(o);
+
+    public override void Write(object o, string category)
+    {
+        switch (category)
+        {
+            case DebugUtils.LogCategory:
+                UnityDebug.Log(o);
+                break;
+
+            case DebugUtils.WarningCategory:
+                UnityDebug.LogWarning(o);
+                break;
+
+            case DebugUtils.ErrorCategory:
+                UnityDebug.LogError(o);
+                break;
+
+            default:
+                UnityDebug.Log(o);
+                break;
+        }
+    }
+
+    public override void Write(string message, string category)
+    {
+        switch (category)
+        {
+            case DebugUtils.LogCategory:
+                UnityDebug.Log(message);
+                break;
+
+            case DebugUtils.WarningCategory:
+                UnityDebug.LogWarning(message);
+                break;
+
+            case DebugUtils.ErrorCategory:
+                UnityDebug.LogError(message);
+                break;
+
+            default:
+                UnityDebug.Log(message);
+                break;
+        }
+    }
+
+    public override void WriteLine(string message) => UnityDebug.Log(message);
+
+    public override void WriteLine(object o) => UnityDebug.Log(o);
+
+    public override void WriteLine(object o, string category)
+    {
+        switch (category)
+        {
+            case DebugUtils.LogCategory:
+                UnityDebug.Log(o);
+                break;
+
+            case DebugUtils.WarningCategory:
+                UnityDebug.LogWarning(o);
+                break;
+
+            case DebugUtils.ErrorCategory:
+                UnityDebug.LogError(o);
+                break;
+
+            default:
+                UnityDebug.Log(o);
+                break;
+        }
+    }
+
+    public override void WriteLine(string message, string category)
+    {
+        switch (category)
+        {
+            case DebugUtils.LogCategory:
+                UnityDebug.Log(message);
+                break;
+
+            case DebugUtils.WarningCategory:
+                UnityDebug.LogWarning(message);
+                break;
+
+            case DebugUtils.ErrorCategory:
+                UnityDebug.LogError(message);
+                break;
+
+            default:
+                UnityDebug.Log(message);
+                break;
+        }
+    }
+}
---
+++ Scripts/Debugging/DebugListenerAdder.cs
@@ -0,0 +1,18 @@
+﻿using Autrage.LEX.NET;
+using System.Linq;
+using UnityEngine;
+using SystemDebug = System.Diagnostics.Debug;
+
+internal class DebugListenerAdder : MonoBehaviour
+{
+    private void Start()
+    {
+        if (!SystemDebug.Listeners.OfType<DebugListener>().Any())
+        {
+            SystemDebug.Listeners.Add(new DebugListener());
+            DebugUtils.Log("Added debug listener.");
+        }
+
+        Destroy(this);
+    }
+}

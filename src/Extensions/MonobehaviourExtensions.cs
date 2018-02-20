ef44cd06fe4bb63cff2543c75894135ba1331577
Markus Rezai <markus.rezai@gmail.com>
2018-02-20 15:37:49 +0100

    Added some extension methods

---
+++ src/Extensions/MonobehaviourExtensions.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using UnityEngine;
+
+public static class MonobehaviourExtensions
+{
+    public static T AddComponent<T>(this MonoBehaviour mono) where T : Component => mono.gameObject.AddComponent<T>();
+
+    public static Component AddComponent(this MonoBehaviour mono, Type componentType) => mono.gameObject.AddComponent(componentType);
+}
